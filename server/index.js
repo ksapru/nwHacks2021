64f28bcd93c7ff3296a63608cd98a6d1a973a8f5
Corbyn Kwan <corbynkwan@gmail.com>
2021-01-10 07:05:47 +0800

    'added_get_request'

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json()); //req.body
 
+app.get("/calculate", async (req, res) => {
+    let {budget, worklocation} = req.body;
+    console.log({
+      budget, worklocation
+    })
+    res.status(400).json({budget,worklocation})
+  });
 //get, put, post, delete stuff
 app.listen(5000, () => {
     console.log("server has started on port 5000");
