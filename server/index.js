8baea407a6a9e0985fd20debbebf56585cb373d8
Corbyn Kwan <corbynkwan@gmail.com>
2021-01-10 05:26:21 +0800

    'server_boilerplate'

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//const pool = require("./db");
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//get, put, post, delete stuff
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
+  });
