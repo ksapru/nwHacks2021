810861c4f5206797d23315a6096c220c546ed602
scottlangille <scottrlangille@gmail.com>
2021-01-09 21:19:23 -0800

    Restructured landing page
    Added logo;
    Changed font sizes and weights;
    Using flexbox;
    Changed images

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,19 +6,31 @@ import Button from '@material-ui/core/Button';
 export default function Login() {
     return (
         <div className="Login">
-            <h2><center>Welcome to Vancouver</center></h2> 
-            <h4> Find a neighbourhood to settle in to </h4><input></input>  <p></p>
+            <a href="#"><img id="logo-full" src="/images/logo.png" alt="Find my Neighbourhood"></img></a>
+
+            <div class="container">
+                <div class="welcome">
+                    <h2>WELCOME TO VANCOUVER</h2> 
+                    <h4>Find a neighbourhood to settle into.</h4>
+                </div>
             
-             <form>
+                <form>
+                    <h5>Help us find your new home</h5>
+                    <label for="">Price Range</label><br></br>
+                    <select><option>Low</option><option>High</option></select><br></br>
+                    <label for="">Work/School Location</label><br></br>
+                    <input></input><br></br>
 
-        
-             <h4>Importance of safety: <select><option>Low</option><option>High</option></select> </h4>   <p></p> 
-             <h4>Importance of public transit: <select><option>Low</option><option>High</option></select></h4><p></p>   
-             <h4>Number of food/restaurants: <select><option>10</option><option>High</option></select></h4> <p></p>  
-                
-             <center> <h4> If this is a new user, click here: </h4> <button message="newUser" value="newUser"> Create an account </button></center>
-
-             </form>
+                    <h5>How much does the following matter to you?</h5>
+                    <label for="">Safety</label><br></br>
+                    <select><option>Low</option><option>High</option></select><br></br>
+                    <label for="">Public transit</label><br></br>
+                    <select><option>Low</option><option>High</option></select><br></br>
+                    <label for="">Restaurants</label><br></br>
+                    <select><option>10</option><option>High</option></select><br></br>
+                        
+                </form>
+             </div>
         </div>
     )
 }
