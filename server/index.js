7c6c70c81645271f0704f027a508c8447046a60f
Corbyn Kwan <corbynkwan@gmail.com>
2021-01-10 11:36:50 +0800

    'with_restaurant_script'

--- server/index.js
+++ server/index.js
@@ -4,8 +4,39 @@ const cors = require("cors");
 const calculateSafety  = require("./calculateSafety");
 const calculateRestaurants = require("./calculateRestaurants")
 require('dotenv').config();
-
-
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'C:/Users/corby/nwHacks2021/server/restaurants.csv',
+    header: [
+        {id: 'neighbourhood', title: 'NEIGHBOURHOOD'},
+        {id: 'rating', title: 'RATING'}
+    ]
+});
+let neighbourhoodStrings=[
+"arbutus ridge",  
+"central business district",
+"dunbar - southlands", 
+"fairview", 
+"grandview - woodland",
+"hastings - sunrise", 
+"kensington - cedar cottage", 
+"kerrisdale", 
+"killarney", 
+"kitsilano", 
+"marpole", 
+"mount pleasant", 
+"musqueam",  
+"oakridge", 
+"renfrew - collingwood", 
+"riley park", 
+"shaughnessy", 
+"south cambie",   
+"stanley park", 
+"strathcona", 
+"sunset", 
+"victoria - fraserview", 
+"west end", 
+"west point grey"]
 //const pool = require("./db");
 //middleware
 app.use(cors());
@@ -16,13 +47,18 @@ app.get("/calculate", async (req, res) => {
     console.log({
       budget, worklocation
     })
-    let a= calculateRestaurants("central business district");
-    
-    console.log("asd",a)
-    //console.log(ratingSum)
-    safetyStars = calculateSafety(worklocation)
-    res.status(400).json(safetyStars)
-  });
+    const records = [] ;
+    for(const neighbourhood of neighbourhoodStrings) { 
+        let {numberOfRestaurants,averageRating} = await calculateRestaurants(neighbourhood)
+        records.push({neighbourhood,rating: averageRating * 2})
+    }
+    console.log(records)
+    csvWriter.writeRecords(records)       // returns a promise
+    .then(() => {
+        console.log('...Done');
+    });
+    res.status(400).json("ok")
+});
 //get, put, post, delete stuff
 app.listen(5000, () => {
     console.log("server has started on port 5000");
