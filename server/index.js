7553d1b57913b545de0feb2ef2370494305fb7dd
Corbyn Kwan <corbynkwan@gmail.com>
2021-01-10 11:38:21 +0800

    'csv_added_script_removed'

--- server/index.js
+++ server/index.js
@@ -4,39 +4,7 @@ const cors = require("cors");
 const calculateSafety  = require("./calculateSafety");
 const calculateRestaurants = require("./calculateRestaurants")
 require('dotenv').config();
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-    path: 'C:/Users/corby/nwHacks2021/server/restaurants.csv',
-    header: [
-        {id: 'neighbourhood', title: 'NEIGHBOURHOOD'},
-        {id: 'rating', title: 'RATING'}
-    ]
-});
-let neighbourhoodStrings=[
-"arbutus ridge",  
-"central business district",
-"dunbar - southlands", 
-"fairview", 
-"grandview - woodland",
-"hastings - sunrise", 
-"kensington - cedar cottage", 
-"kerrisdale", 
-"killarney", 
-"kitsilano", 
-"marpole", 
-"mount pleasant", 
-"musqueam",  
-"oakridge", 
-"renfrew - collingwood", 
-"riley park", 
-"shaughnessy", 
-"south cambie",   
-"stanley park", 
-"strathcona", 
-"sunset", 
-"victoria - fraserview", 
-"west end", 
-"west point grey"]
+
 //const pool = require("./db");
 //middleware
 app.use(cors());
@@ -47,16 +15,7 @@ app.get("/calculate", async (req, res) => {
     console.log({
       budget, worklocation
     })
-    const records = [] ;
-    for(const neighbourhood of neighbourhoodStrings) { 
-        let {numberOfRestaurants,averageRating} = await calculateRestaurants(neighbourhood)
-        records.push({neighbourhood,rating: averageRating * 2})
-    }
-    console.log(records)
-    csvWriter.writeRecords(records)       // returns a promise
-    .then(() => {
-        console.log('...Done');
-    });
+
     res.status(400).json("ok")
 });
 //get, put, post, delete stuff
