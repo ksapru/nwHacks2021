83ea9fdde514e09c09e62f0d40c3d33117739a22
Krish <67235656+ksapru@users.noreply.github.com>
2021-01-10 10:43:59 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Product from "./Product";
+import {Box, Button} from '@material-ui/core';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
-
-    
-      <Login></Login>
-
     
 
-      
+    
+      <Login></Login>   
 
 
+      <Switch> <Route to="/Product" exact component={Product} /> <Route path="/" exact component={Login} /> Find my neighbourhood </Switch>
+      <h2>Top 5 neighbourhoods for you</h2>
 
+         <Product></Product>
+         <Product></Product>
+         <Product></Product>
+         <Product></Product>
+         
 
       </div>
+    </Router>
     
 
   );
