f0b4bd71807921f7c78791fdcf42bf3e1049cbd5
Corbyn Kwan <corbynkwan@gmail.com>
2021-01-10 10:46:24 +0800

    'added_dotaenv'

--- server/calculateRestaurants.js
+++ server/calculateRestaurants.js
@@ -6,11 +6,18 @@ module.exports = (neighbourhood) => {
     const client = new Client({});
     axios({
       method: 'get',
-      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+${neighbourhood}&location=49.210724,-123.130188&radius=10000&types=restaurant&rankby=prominence&key=AIzaSyBN-304ky5ok_xF0xY2XATTwjA4YxRj7jg`
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+${neighbourhood}&location=49.210724,-123.130188&radius=10000&types=restaurant&rankby=prominence&key=${process.env.GOOGLE_API_KEY}`
     })
       .then(function (response) {
-        console.log(response.data.results.length)
-        console.log(`restaurants in ${neighbourhood}`)
+        let ratingSum = 0;
+        for (const restaurant of response.data.results) {
+          ratingSum+=restaurant.rating;
+        }
+        let numberOfRestaurants = response.data.results.length
+        rating = ratingSum / response.data.results.length
+        console.log(numberOfRestaurants)
+        console.log(ratingSum)
+        return {"numberOfRestaurants": numberOfRestaurants,"ratingSum": ratingSum};
       });
 
 
--- server/index.js
+++ server/index.js
@@ -3,17 +3,23 @@ const app = express();
 const cors = require("cors");
 const calculateSafety  = require("./calculateSafety");
 const calculateRestaurants = require("./calculateRestaurants")
+require('dotenv').config();
+
+
 //const pool = require("./db");
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
-
+//ss
 app.get("/calculate", async (req, res) => {
     let {budget, worklocation} = req.body;
     console.log({
       budget, worklocation
     })
-    calculateRestaurants("central business district");
+    let a= calculateRestaurants("central business district");
+    
+    console.log("asd",a)
+    //console.log(ratingSum)
     safetyStars = calculateSafety(worklocation)
     res.status(400).json(safetyStars)
   });
