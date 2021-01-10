3eb5a70b92a7abb7dc52c00348eadd61ade15d40
Krish <67235656+ksapru@users.noreply.github.com>
2021-01-10 16:29:05 +0400

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -9,6 +9,7 @@ import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import {BrowserRouter as Route} from 'react-router-dom';
+import 'axios' from './axios';
 
 const useStyles = makeStyles((theme) => ({
   }));
@@ -23,14 +24,38 @@ const marks = [
       label: "$2000/mo",
     },
 ]
-
+const handleCalculations = (budget,workLocation) => async () => {
+    try {
+      const response = await axios.post('/calculate', {budget,workLocation});
+      
+    // response type array
+    /*[
+      {
+        "neighbourhood": "University Endowment Lands",
+        "match": 99.1,
+        "safety": 9.9,
+        "transit": 7.1,
+        "food": 4.5
+      },
+      {
+      "neighbourhood": "West Point Grey",
+      "match": 92.1,
+      "safety": 9.9,
+      "transit": 7.2,
+      "food": 4.1
+      }, ...
+    */
+  } catch (err) {
+      console.log("got an error")
+  }
+}
 function valuetext(value) {
     return `$${value.map(0,100,600,2000)}/mo`;
 }
 
 export default function Login() {
     const classes = useStyles();
-
+    const [safety, setSafety] = useState(0);
     return (
         <div className="Login">
             <a href="#"><img id="logo-full" src="/images/logo.png" alt="Find my Neighbourhood"></img></a>
@@ -53,7 +78,7 @@ export default function Login() {
 
                     <h5>How much does the following matter to you?</h5>
                     <label for="safety-rating">Safety</label><br></br>
-                    <Slider id="safety-rating" defaultValue={10} min={0} max={100} color={"#4B8A44"} decimals={0}></Slider>
+                    <Slider id="safety-rating"  onChange={(event, value) => setSafety(value)} defaultValue={10} min={0} max={100} color={"#4B8A44"} decimals={0}></Slider>
 
                     <label for="transit-rating">Public transit</label><br></br>
                     <Slider id="transit-rating" defaultValue={10} min={0} max={100} color={"#4B8A44"} decimals={0}></Slider>
@@ -62,12 +87,12 @@ export default function Login() {
                     <Slider id="food-rating" defaultValue={10} min={0} max={100} color={"#4B8A44"} decimals={0}></Slider>
 
                     <Link to='/Product'>
-                        <button type="button" id="search">Find my neighbourhood</button>
+                        <button type="button" id="search" onClick={() => handleCalculations(budget,workLocation)}>Find my neighbourhood</button>
                     </Link>
                     <Route path="/" component="Product" /> 
 
                 </form>
              </div>
         </div>
-    )
+    );
 }
